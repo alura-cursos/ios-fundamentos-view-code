724e6cb6619237bc2e335e329da5162f7cdc52eb
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-10-30 22:03:06 -0300

    aula 1.2 - criando projeto

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Giovanna Moeller on 30/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
