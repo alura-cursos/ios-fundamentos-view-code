692eada53f0831d00c506547541e57083894c602
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-10-30 22:12:42 -0300

    aula 3.1 - criando uma UILabel

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -12,7 +12,15 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .background
+        
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        
+        view.addSubview(label)
+        
     }
 
 
