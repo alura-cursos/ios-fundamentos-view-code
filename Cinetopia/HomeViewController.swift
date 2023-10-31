b43a5e6e1f96cdda85bc76bf2e8a8fc845ca9d58
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-10-30 22:15:46 -0300

    aula 3.4 - boas práticas do mercado

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -8,27 +8,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .background
-        
+    
+    private lazy var label: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Hello, World!"
         label.textColor = .white
         label.font = .systemFont(ofSize: 24.0, weight: .bold)
-        
-        view.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
-        ])
-        
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .background
+        addSubviews()
+        setupConstraints()
     }
-
-
+    
+    private func addSubviews() {
+        view.addSubview(label)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        ])
+    }
+    
+    
 }
 
