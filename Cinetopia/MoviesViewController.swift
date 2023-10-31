65bced2c8919b8cd39ffa5df68e349b88e2c2d2b
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-10-30 22:25:08 -0300

    aula 5.3 - conhecendo propriedades de navegação

--- Cinetopia/MoviesViewController.swift
+++ Cinetopia/MoviesViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class MoviesViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupNavigationBar() {
+        title = "Filmes Populares"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationItem.setHidesBackButton(true, animated: true)
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
