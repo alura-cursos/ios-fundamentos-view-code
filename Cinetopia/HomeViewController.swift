18ed058857a94253d62103c622caba34f7422e4f
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-10-30 22:09:29 -0300

    aula 2.2 - adaptando projeto para view code

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Cinetopia
 //
 //  Created by Giovanna Moeller on 30/10/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
